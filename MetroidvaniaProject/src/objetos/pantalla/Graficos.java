a0a5afc906ff85d4b6a9fa8505f0eeb39c809398
Alvaro Garcia Diego <alvaro.gd@opendeusto.es>
2021-11-06 13:39:17 +0100

    graficos.java comentado

--- MetroidvaniaProject/src/objetos/pantalla/Graficos.java
+++ MetroidvaniaProject/src/objetos/pantalla/Graficos.java
@@ -2,13 +2,24 @@ package objetos.pantalla;
 
 import java.io.*;
 
+/**
+ * Clase Madre Graficos de la cual heredaran los objetos en pantalla
+ */
 public class Graficos implements Serializable {
-	private static final long serialVersionUID = 1L;
-	protected int posX;
-	protected int posY;
-	protected int altura;
-	protected int anchura;
+	private static final long serialVersionUID = 1L; //Version Serializable
+	protected int posX; //Posicion X en pantalla
+	protected int posY; //Posicion Y en pantalla
+	protected int altura; //Altura en pantalla
+	protected int anchura; //Anchura en pantalla
 	
+	
+	/**
+	 * @param x Posicion X en pantalla
+	 * @param y Posicion Y en pantalla
+	 * @param alt Altura en pantalla
+	 * @param anch Anchura en pantalla
+	 * Crea un objeto de tipo grafico con unos valores introducidos
+	 */
 	protected Graficos(int x, int y, int alt, int anch) {
 		this.setPosX(x);
 		this.setPosY(y);
@@ -16,6 +27,7 @@ public class Graficos implements Serializable {
 		this.setAnchura(anch);
 	}
 	
+	//SEtters y Getters basicos
 	protected int getPosX() {
 		return posX;
 	}
@@ -48,9 +60,12 @@ public class Graficos implements Serializable {
 		this.anchura = anchura;
 	}
 	
+	//Metodo mover vacio
 	protected void mover() {}
 	
+	//Metodo saltar vacio
 	protected void saltar() {}
 	
+	//Metodo animar vacio
 	protected void animar() {}
 }
