60fc5d166644e1c54c9010722c265358e556cd43
Nicolás <nicolas.perdiguero@opendeusto.es>
2021-11-07 20:18:29 +0100

    Mejora de la interfaz de los botones

--- MetroidvaniaProject/src/ventanas/VentanaMenuInicio.java
+++ MetroidvaniaProject/src/ventanas/VentanaMenuInicio.java
@@ -1,31 +1,25 @@
 package ventanas;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.border.Border;
+
 
 //Ventana que se mostrará al ejecutar el juego 
 //Permite iniciar la partida, ver los controles del juego, volver al escritorio y ver el ranking
 public class VentanaMenuInicio extends JFrame{
 	//Atributos de la clase
 		//Variables que indicarán el tamaño de la ventanas
-	private static int x = 2000;	
-	private static int y = 1000;
-		//Variables que situarán los botones en el centro de la pantalla
-	
-
+	private static int x = 1800;	
+	private static int y = 900;
+		//Variables para los botones
+	private static int lx = 700;
+	private static int ly = 100;
+	private static int espacio = 200;
+	private static int altura = 100;
+	private static int anchura = 500;
+	private static Color color = Color.LIGHT_GRAY; 
+	private static Font letra = new Font("Arial", Font.BOLD, 17);
 		//Imagenes 
 	private Image logo = new ImageIcon(getClass().getResource("/images/Hexagrama.jpg")).getImage();	
 	
@@ -38,15 +32,13 @@ public class VentanaMenuInicio extends JFrame{
 	public VentanaMenuInicio() {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		Dimension frameSize = this.getSize();
-		
-		int lx = (screenSize.width-frameSize.width)/2;
-		int ly = (screenSize.height-frameSize.height)/2;
+
 
 		//Características de la ventana
 		 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setSize(x, y);
-		setMinimumSize(new Dimension(900, 600));
+		setMinimumSize(new Dimension(x, y));
 		this.setLocationRelativeTo(null);
 		this.setTitle("El sueño de kerman");
 		 
@@ -66,36 +58,39 @@ public class VentanaMenuInicio extends JFrame{
 		panelBotones.setLayout(new GridLayout(8, 1));
 		panelBotones.setOpaque(false);
 		p.setOpaque(false);
+
+			//Situar los botones y establecer su tamaño
+		bNuevaPartida.setBounds(lx, ly, anchura,altura );
+		ly += espacio;
+		bRanking.setBounds(lx, ly, anchura, altura);
+		ly += espacio;
+		bControles.setBounds(lx, ly, anchura, altura);
+		ly += espacio;
+		bExit.setBounds(lx, ly, anchura, altura);
 		
-		
-		bNuevaPartida.setOpaque(false);
-		bNuevaPartida.setSize(400, 200);
-		bRanking.setOpaque(false);
-		bControles.setOpaque(false);
-		bExit.setOpaque(false);
+			//Borde de los botones
 		bNuevaPartida.setBorder(bordeBoton);
 		bRanking.setBorder(bordeBoton);
 		bControles.setBorder(bordeBoton);
 		bExit.setBorder(bordeBoton);
 		
-		 
-		//Añadir elementos a los paneles
-		 
-		panelBotones.add(bNuevaPartida);
-		panelBotones.add(p);
-		panelBotones.add(bRanking);
-		panelBotones.add(p);
-		panelBotones.add(bControles);
-		panelBotones.add(p);
-		panelBotones.add(bExit);
-		 
-		//Interfaz del Menu
-		 
+			//Fuente de los botones
+		bNuevaPartida.setFont(letra);
+		bRanking.setFont(letra);
+		bControles.setFont(letra);
+		bExit.setFont(letra);
+			//Color de los botones
+		bNuevaPartida.setBackground(color);
+		bRanking.setBackground(color);
+		bControles.setBackground(color);
+		bExit.setBackground(color);
 		//Añadir componentes a la ventana
 		 
 		setIconImage(logo);
-		panelCentral.add(panelBotones, BorderLayout.CENTER);
-
+		add(bNuevaPartida);
+		add(bRanking);
+		add(bControles);
+		add(bExit);
 		add(panelCentral);
 		
 		 	 
