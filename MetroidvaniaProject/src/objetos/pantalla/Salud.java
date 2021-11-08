239d8a4cdbb09a4ad1b4bd545d511c2aba738aed
Kerman <kerman.urrutikoetxea@opendeusto.es>
2021-11-08 19:56:24 +0100

    Parte de Miguel

---
+++ MetroidvaniaProject/src/objetos/pantalla/Salud.java
@@ -0,0 +1,26 @@
+package objetos.pantalla;
+
+import utils.ventanas.ventanaBitmap.VentanaGrafica;
+
+public class Salud extends Objeto {
+	//imports correspondientes, los atributos (ninguno que no este en la clase padre) y el super
+	public Salud(int x, int y, int velocidadX, int velocidadY, int radioHitbox, String imagen) {
+		super(x, y, velocidadX, velocidadY, radioHitbox, imagen);
+	}
+	
+	public Salud(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.velocidadX = 0;
+		this.velocidadY = 0;
+		this.radioHitbox = 0;
+		this.imagen = "magia.png";
+	}
+
+	public void mover(VentanaGrafica juego) {
+		//movimientos lineales en los ejes x e y, por defecto esta hecho para que dispare hacia arriba
+		this.x += this.velocidadX;
+		this.y += this.velocidadY;
+	}
+
+}
