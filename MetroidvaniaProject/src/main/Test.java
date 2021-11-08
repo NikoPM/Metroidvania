2019902e3ff986759ddf130a8d45e458ca823cd7
Alvaro Garcia Diego <alvaro.gd@opendeusto.es>
2021-11-08 18:24:42 +0100

    edit

--- MetroidvaniaProject/src/main/Test.java
+++ MetroidvaniaProject/src/main/Test.java
@@ -16,8 +16,8 @@ public class Test {
 			this.setAlwaysOnTop(true);
 			this.setMinimumSize(new Dimension(500, 500));
 			this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-			Consumibles.generar(40, 100, "imagenes/pelota.png", this);
-			Consumibles.generar(30, 10, "imagenes/pelota.png", this);
+			Consumibles.generar(40, 100, "src/images/pelota.png", this);
+			Consumibles.generar(30, 10, "src/images/pelota.png", this);
 			this.setVisible(true);
 		}	
 	}
