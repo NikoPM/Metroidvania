0b8452e5fd6eddee51991360676712eab16a86f6
Nicolás <nicolas.perdiguero@opendeusto.es>
2021-11-14 18:36:39 +0100

    VentanaJuego creation

--- MetroidvaniaProject/src/ventanas/VentanaJuego.java
+++ MetroidvaniaProject/src/ventanas/VentanaJuego.java
@@ -1,5 +1,18 @@
 package ventanas;
 
-public class VentanaJuego {
+import javax.swing.JFrame;
+
+
+
+public class VentanaJuego extends JFrame{
 	
+	
+	public static void main(String[] args) {
+		VentanaJuego v = new VentanaJuego();
+		v.setVisible(true);
+	}
+	
+	public VentanaJuego() {
+		//TODO Agregar elementos cuando estén todos preparados
+	}
 }
