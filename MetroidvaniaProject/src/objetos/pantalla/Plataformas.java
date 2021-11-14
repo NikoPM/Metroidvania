0d07ce0c2b64e81ac893434d74a46f6d99880e7c
Nicolás <nicolas.perdiguero@opendeusto.es>
2021-11-14 18:25:19 +0100

    Platforms constructor

---
+++ MetroidvaniaProject/src/objetos/pantalla/Plataformas.java
@@ -0,0 +1,16 @@
+package objetos.pantalla;
+
+public class Plataformas extends Graficos {
+	private static final long serialVersionUID = 1L;
+	//TODO Añadir los atributos de altura t anchura de la plataforma cuando Alvaro y Miguel cambien 
+	//algunas cosas de la clase Graficos y otras
+	
+	/**Constructor privado de la clase Platafromas
+	 * @param x	Posicion X de la plataforma en pantalla
+	 * @param y	Posicion Y de la plataforma en pantalla
+	 * @param dir	String con la dirección a la imagen del 
+	 */
+	private Plataformas (int x, int y, String dir) {
+		super(x, y, dir);
+	}
+}
--- MetroidvaniaProject/src/test/MenuIniTest.java
+++ MetroidvaniaProject/src/test/MenuIniTest.java
@@ -3,17 +3,25 @@ package test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import junit.framework.TestCase;
 import ventanas.VentanaMenuInicio;
+import ventanas.VentanaMenuInicio.FondoPanel;
 
 public class MenuIniTest {
+	private VentanaMenuInicio v;
+	
+	//Inicialización de las clases que vamos a testear
+	@Before
+	public void setUp() {
+		v = new VentanaMenuInicio();
+	}
 	
 	@Test
 	public void probar() {
-		VentanaMenuInicio v = new VentanaMenuInicio();
-		assertNotNull(v);
-		//assertNotNull(v.getbExit().getActionListeners());
+		assertNotNull(v);	
 	}
 
 }
