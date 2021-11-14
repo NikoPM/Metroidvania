39d63a1fa505610d107c8a005be78dde175866cb
Alvaro Garcia Diego <alvaro.gd@opendeusto.es>
2021-11-14 18:52:01 +0100

    test

--- MetroidvaniaProject/src/objetos/pantalla/Consumibles.java
+++ MetroidvaniaProject/src/objetos/pantalla/Consumibles.java
@@ -17,8 +17,7 @@ public class Consumibles extends Graficos {
 	private Consumibles(int x, int y, String dir) {
 		super(x, y, dir);
 	}
-	
-	//TODO Comentar  
+	 
 	private static void editarLabel(final JLabel label, final int posX, final int posY) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
