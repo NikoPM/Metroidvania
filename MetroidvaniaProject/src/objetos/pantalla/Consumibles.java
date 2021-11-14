af6c7342e8902f36a9710c0804c0d8140cc2873d
Alvaro Garcia Diego <alvaro.gd@opendeusto.es>
2021-11-14 21:37:01 +0100

    edit a los hilos de la clase consumible y a metodos de este mismo mas comentarios

--- MetroidvaniaProject/src/objetos/pantalla/Consumibles.java
+++ MetroidvaniaProject/src/objetos/pantalla/Consumibles.java
@@ -18,28 +18,47 @@ public class Consumibles extends Graficos {
 		super(x, y, dir);
 	}
 	
-	/** Metodo Estatico Privado EditarLabel
-	 * @param label el label que se edita
-	 * @param posX la posicion en el eje X del label
-	 * @param posY la posicion en el eje Y del label
-	 * Anima el label haciendolo bajar y subir 5 pixeles durante 70*5*2*10 miliseg (por ahora) 
-	 * o hasta que el hilo sea interrumpido
+	/** Metodo Estatico Privado LabelMove
+	 * @param label JLabel que se edita
+	 * @param posX posicion en el eje X del label
+	 * @param posY posicion en el eje X del label
+	 * @param j int que se suma o resta a la posY
+	 * @param b boolean que indica si realizar la operacion de suma o resta
+	 * Establece y edita la posicion del label
 	 */
-	private static void editarLabel(final JLabel label, final int posX, final int posY) {
+	private static void labelMove(final JLabel label, final int posX, final int posY, final int j, final boolean b) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				if(b) {
+					label.setLocation(posX, posY+1*j);
+				} else {
+					label.setLocation(posX, 5+posY-1*j);		
+				}
+			}
+		});
+	}
+	
+	/** Metodo Privado Crear
+	 * @param label JLabel con la imagen del consumible
+	 * @param vent Ventana en la que se crear el consumible
+	 * Añade el consumible al Contentpane de la ventana, edita su layout a flowlayout 
+	 * y crea un hilo que llama a la funcion labelMove que anima el consumible haciendolo 
+	 * subir o bajar 5 pixeles
+	 */
+	private void crear(JLabel label, JFrame vent) {
+		vent.getContentPane().setLayout(new FlowLayout());
+		vent.getContentPane().add(label);
 		Thread hilo = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				for(int i = 0; !Thread.interrupted() && i< 10; i++) {
-					for(int j = 0; j<5; j++) {
-						label.setLocation(posX, posY+1*j);
-						try {
-							Thread.sleep(70);
-						} catch(InterruptedException e) {
-							Thread.currentThread().interrupt();
+					for(int j = 0; j<10; j++) {
+						if(j<5) {
+							labelMove(label, posX, posY, j, true);
+						} else {
+							labelMove(label, posX, posY, j-5, false);
 						}
-					}
-					for(int j = 0; j<5; j++) {
-						label.setLocation(posX, 5+posY-1*j);
 						try {
 							Thread.sleep(70);
 						} catch(InterruptedException e) {
@@ -52,42 +71,24 @@ public class Consumibles extends Graficos {
 		hilo.start();
 	}
 	
-	/** Metodo Privado Crear
-	 * @param vent Ventana en la que se crear el consumible
-	 * @return devuelve el JLabel con la imagen y posicion del consumible
-	 * Añade el consumible al Contentpane de la ventana, edita su layout a flowlayout 
-	 * y llama a la funcion editarLabel que anima el consumible haciendolo 
-	 * subir y bajar 5 pixeles
-	 */
-	private JLabel crear(JFrame vent) {
-		JLabel label = new JLabel(new ImageIcon(this.dirImg));
-		vent.getContentPane().setLayout(new FlowLayout());
-		vent.getContentPane().add(label);
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				editarLabel(label, posX, posY);
-			}
-		});
-		return label;
-	}
-	
 	/** Metodo Estatico Generar
 	  * @param x Posicion X del consumible en pantalla
 	 * @param y Posicion Y del consumible en pantalla
 	 * @param dir Direccion en la que se encuentra la imagen(es) del consumible
 	 * @param vent Ventana en la que se crear el consumible
-	 * @return Crear un objeto de la clase consumible, lo introduce en la ventana animandolo 
-	 * y devuelve el JLabel con la imagen y posicion del consumible
+	 * @return Crear un objeto de la clase consumible y un JLabel del mismo, 
+	 * lo introduce en la ventana animandolo y devuelve el JLabel con la imagen y posicion del consumible
 	 * LLama al constructor y al metodo crear
 	 */
-	public static void generar(int x, int y, String dir, JFrame vent) {
+	public static JLabel generar(int x, int y, String dir, JFrame vent) {
+		Consumibles cons = new Consumibles(x, y, dir);
+		JLabel label = new JLabel(new ImageIcon(cons.dirImg));
 		SwingUtilities.invokeLater(new Runnable() {	
 			@Override
 			public void run() {
-				Consumibles cons = new Consumibles(x, y, dir);
-				cons.crear(vent);
+				cons.crear(label, vent);
 			}
 		});
+		return label;
 	}
 }
