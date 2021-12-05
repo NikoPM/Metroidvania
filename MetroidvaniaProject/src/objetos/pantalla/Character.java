1e9cc718d6c8dd0027aa033f0efb60aa5d38b152
Alvaro Garcia Diego <alvaro.gd@opendeusto.es>
2021-12-05 19:35:13 +0100

    clase chracter animada y testeada

--- MetroidvaniaProject/src/objetos/pantalla/Character.java
+++ MetroidvaniaProject/src/objetos/pantalla/Character.java
@@ -1,14 +1,54 @@
 package objetos.pantalla;
 
-import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.*;
 
 public class Character extends Objeto {
 	
-	public Character(int x, int y, int velocidadX, int velocidadY, int radioHitbox, String imagen) {
-		super(x, y, velocidadX, velocidadY, radioHitbox, imagen);
+	private static final int VEL_X = 0;
+	private static final int VEL_Y = 0;
+	private static final int HITBOX = 0;
+	
+	private Character(int x, int y, String imagen) {
+		super(x, y, VEL_X, VEL_Y, HITBOX, imagen);
 	}
 	
-	public void mover() {
-		
+	/** Metodo Privado Generar
+	 * @param vent Ventana en la que se crear el personaje
+	 */
+	public static JLabel generar(int x, int y, String imagen, JFrame vent) {
+		Character charac = new Character(x, y, imagen);
+		vent.getContentPane().setLayout(new FlowLayout());
+		JLabel label = new JLabel(new ImageIcon(charac.dirImg));
+		vent.getContentPane().add(label);
+		return label;
+	}
+	
+	private static void moverCharacter(final JLabel label , final boolean eje) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				if(eje) {
+					label.setLocation(label.getX() + 1, label.getY());
+				} else {
+					label.setLocation(label.getX(), label.getY() + 1);
+				}
+			}
+		});
+	}
+	
+	public static Thread mover(JLabel label, boolean eje) {
+		Thread hilo = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0; i<10; i++) {
+					try {
+						moverCharacter(label, eje);
+						Thread.sleep(100);
+					} catch (InterruptedException e) {}
+				}
+			}
+		});
+		return hilo;
 	}
 }
