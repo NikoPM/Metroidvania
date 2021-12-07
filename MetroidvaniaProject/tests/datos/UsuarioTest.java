2e14a9e0d7912c4faa1e67f6c8fdb8c83f228c95
Kerman <kerman.urrutikoetxea@opendeusto.es>
2021-12-07 13:33:25 +0100

    JUnit de Usuarios

---
+++ MetroidvaniaProject/tests/datos/UsuarioTest.java
@@ -0,0 +1,25 @@
+package datos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import objetos.pantalla.Graficos;
+
+public class UsuarioTest {
+
+	private static Usuario usuario = new Usuario(1, "Andoni", 200);
+	
+	@Test
+	public void testGetIdUsuario() {
+		assertEquals(1, usuario.getIdUsuario());
+	}
+	@Test
+	public void testGetNombre() {
+		assertEquals("Andoni", usuario.getNombre());
+	}
+	@Test
+	public void testGetTiempo() {
+		assertEquals(200, usuario.getTiempo(), 1);
+	}
+}
