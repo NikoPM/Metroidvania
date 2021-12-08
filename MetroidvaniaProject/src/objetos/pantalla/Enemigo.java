6b391c8cc9e2a018d82904b8fd4eae7a567f74c9
MiguelSolana <miguelsolana@opendeusto.es>
2021-12-08 22:41:19 +0100

    clases borradas

--- MetroidvaniaProject/src/objetos/pantalla/Enemigo.java
+++ MetroidvaniaProject/src/objetos/pantalla/Enemigo.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
-import utils.ventanas.ventanaBitmap.VentanaGrafica;
+
 
 public class Enemigo extends Graficos /** implements Destruible **/
 {
