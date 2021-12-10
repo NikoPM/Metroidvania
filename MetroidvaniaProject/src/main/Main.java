3981f97bde638cf561abfedc80bfea8a754d2cfb
NikoPM <nicolas.perdiguero@opendeusto.es>
2021-12-10 16:36:29 +0100

    Hilo en main

--- MetroidvaniaProject/src/main/Main.java
+++ MetroidvaniaProject/src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
 import ventanas.*;
 
 public class Main {
 	public static void main(String[] args) {
-		new VentanaMenuInicio();
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new VentanaMenuInicio();
+			}
+		});
 	} 
- 
 }
